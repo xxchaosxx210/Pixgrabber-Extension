fd340c1c2b88349c6095a92cf72eafb6e3f896c2
xxchaosxx210 <pmillar1279@googlemail.com>
2021-02-24 21:50:49 +0000

    all working
    just needs tweaking  now

--- background.js
+++ background.js
@@ -7,7 +7,21 @@ function onInstall(){
 }
 
 function todoCallback(jstring){
-    window.alert(jstring);
+    if(jstring){
+        b = btoa(jstring);
+        xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://localhost:5000/set-html", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function(){
+            if(this.readyState == XMLHttpRequest.DONE && this.status == 200){
+                console.log("Connected to PixGrabber successful");
+            }
+            else{
+                window.alert("Unable to connect to PixGrabber. Make sure you have PixGrabber installed and running. Find it here: https://github.com/xxchaosxx210/wxPixGrabber.git")
+            }
+        }
+        xhr.send(b);
+    }
 }
 
 chrome.browserAction.onClicked.addListener(function(tab){
--- content.js
+++ content.js
@@ -1,26 +1,61 @@
+const DIV_CLASSNAME = "pixgrabber_holder";
+const CHK_CLASSNAME = "pixgrabber_checkbox";
 
-function getThumbnailsFromPage(){
-    links = []
-    // find thumbnail images
-    var alist = document.getElementsByTagName("a");
-    for(aindex=0;aindex<alist.length;aindex++){
-        atag = alist[aindex];
-        if(atag.hasChildNodes()){
-            if(atag.firstChild.tagName == "IMG"){
-                links.push({"href": atag.href, "src": atag.firstChild.src});
+function createThumbnailGroups(){
+    groups = [];
+    group = [];
+    for(let element of document.getElementsByTagName("a")){
+        if(element.tagName == "A"){
+            if((element.hasChildNodes()) && element.firstChild.tagName == "IMG"){
+                if(group.length == 0){
+                    // create new div tag
+                    div = document.createElement("div");
+                    div.className = DIV_CLASSNAME;
+                }
+                parent = element.parentNode;
+                parent.replaceChild(div, element);
+                div.appendChild(element);
+                group.push([element, element.firstChild]);
+            }
+            else{
+                if(group.length > 0){
+                    // add checkbox to end of div
+                    var chk = document.createElement("input");
+                    chk.setAttribute("type", "checkbox");
+                    chk.className = CHK_CLASSNAME;
+                    div.appendChild(chk);
+                    groups.push(group);
+                    group = [];
+                }
             }
         }
     }
-    return links
+}
+
+function getSelected(){
+    selected = []
+    for(let div of document.getElementsByClassName(DIV_CLASSNAME)){
+        let chks = div.getElementsByClassName(CHK_CLASSNAME);
+        if(chks){
+            let checkbox = chks[0];
+            if(checkbox.checked){
+                for(let a of div.getElementsByTagName("a")){
+                    selected.push({"href": a.href, "src": a.firstChild.src});
+                }
+            }
+        }
+    }
+    return selected;
 }
 
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse){
     if(msg.text == "report_back"){
-        l = getThumbnailsFromPage();
-        for(i=0;i<l.length;i++){
-            console.log(l[i].src)
-        }
-        var j = {"links": l, "title": document.title, "url": window.location.href};
+        selected = getSelected();
+        var j = {"links": selected, "title": document.title, "url": window.location.href};
         sendResponse(JSON.stringify(j));
     }
+});
+
+window.addEventListener("load", function(event){
+    createThumbnailGroups();
 });
