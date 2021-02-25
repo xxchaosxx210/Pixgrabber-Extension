f61d9ee498db55560b09210e0869853135407e76
xxchaosxx210 <pmillar1279@googlemail.com>
2021-02-25 10:52:49 +0000

    removed checkbox

--- content.js
+++ content.js
@@ -1,8 +1,25 @@
 const DIV_CLASSNAME = "pixgrabber_holder";
-const CHK_CLASSNAME = "pixgrabber_checkbox";
+const OVERLAY_CLASSNAME = "pixgrabber_overlay";
+
+const STYLE_SELECTED_ON = "10px solid #0033cc";
+const STYLE_SELECTED_OFF = "10px solid #324d88";
+
+function onDivClick(event){
+    div = event.currentTarget;
+    var checked = div.checked;
+    console.log("div is set to " + checked);
+    div.checked = !checked;
+    if(div.checked == true){
+        div.style.border = STYLE_SELECTED_ON;
+        div.getElementsByClassName(OVERLAY_CLASSNAME)[0].style.display = "block";
+    }
+    else{
+        div.style.border = STYLE_SELECTED_OFF;
+        div.getElementsByClassName(OVERLAY_CLASSNAME)[0].style.display = "none";
+    }
+}
 
 function createThumbnailGroups(){
-    groups = [];
     group = [];
     for(let element of document.getElementsByTagName("a")){
         if(element.tagName == "A"){
@@ -11,20 +28,23 @@ function createThumbnailGroups(){
                     // create new div tag
                     div = document.createElement("div");
                     div.className = DIV_CLASSNAME;
+                    div.style.border = STYLE_SELECTED_OFF;
                 }
                 parent = element.parentNode;
                 parent.replaceChild(div, element);
                 div.appendChild(element);
+                element.addEventListener("click", function(evt){
+                    evt.preventDefault();
+                });
+                div.setAttribute("checked", false);
+                div.addEventListener("click", onDivClick, false);
                 group.push([element, element.firstChild]);
             }
             else{
                 if(group.length > 0){
-                    // add checkbox to end of div
-                    var chk = document.createElement("input");
-                    chk.setAttribute("type", "checkbox");
-                    chk.className = CHK_CLASSNAME;
-                    div.appendChild(chk);
-                    groups.push(group);
+                    var overlay = document.createElement("div");
+                    overlay.className = OVERLAY_CLASSNAME;
+                    div.appendChild(overlay);
                     group = [];
                 }
             }
@@ -35,13 +55,9 @@ function createThumbnailGroups(){
 function getSelected(){
     selected = []
     for(let div of document.getElementsByClassName(DIV_CLASSNAME)){
-        let chks = div.getElementsByClassName(CHK_CLASSNAME);
-        if(chks){
-            let checkbox = chks[0];
-            if(checkbox.checked){
-                for(let a of div.getElementsByTagName("a")){
-                    selected.push({"href": a.href, "src": a.firstChild.src});
-                }
+        if(div.checked){
+            for(let a of div.getElementsByTagName("a")){
+                selected.push({"href": a.href, "src": a.firstChild.src});
             }
         }
     }
